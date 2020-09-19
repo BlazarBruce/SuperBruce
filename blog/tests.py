eccb2a007e1ed2cf68aff9b85cdb363a01e1c864
zhuchaoqiang <zhu.chaoqiang@byd.com>
2020-09-19 22:13:26 +0800

    # ORM,完成网站注册功能

--- blog/tests.py
+++ blog/tests.py
@@ -1,3 +1,6 @@
 from django.test import TestCase
 
 # Create your tests here.
+import datetime
+curr_time = datetime.datetime.now()
+print(curr_time)
--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,7 @@
 import json
 from blog import models
 import functools
+import datetime
 import requests
 from django.conf import settings
 from django.http import JsonResponse
@@ -39,6 +40,7 @@ def login(request):
 def register(request):
     """网站注册功能"""
     if request.method == "POST":
+        # 获取前端传来的数据
         user = request.POST.get('username')
         pwd = request.POST.get('password')
         email = request.POST.get('email')
@@ -48,8 +50,25 @@ def register(request):
         birthday = request.POST.get('birthday')
         # print(user,pwd,email,name,phone,gender,birthday) # 目前可以拿到前端纯回来发的数据
         # ORM将数据插入数据库、并且返回登录界面
-
-
+        # 查询是否村子啊该用户
+        user_list = models.useInfo.objects.filter(username=user)
+        # 判断数据库是否存在改用户
+        if user_list :
+                error_name = '%s用户名已经存在了' % user
+                return  render(request,'register.html')
+                # return  render(request,'register.html',{'error_name':error_name})
+        # 存储到数据库
+        else:
+            user = models.useInfo.objects.create(username=user,
+                                                 password=pwd,
+                                                 email=email,
+                                                 name=name,
+                                                 phone=phone,
+                                                 gender=gender,
+                                                 birthday=birthday,
+                                                 create_time=datetime.datetime.now())
+            user.save()
+            return redirect('/login/')
     return render(request, 'register.html')
 
 def index(request):
